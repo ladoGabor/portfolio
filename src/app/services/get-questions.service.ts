dbb9645dc09a73fe32c44d777a320c8fe1413622
Gabor <lado.gabor@gmail.com>
2023-04-24 20:49:19 +0200

    w2w: Corrected template - component relations. Corrected get-questions service

--- src/app/services/get-questions.service.ts
+++ src/app/services/get-questions.service.ts
@@ -1,3 +1,7 @@
+// This service loads the full collection of questions into the app from a JSON file
+/* This service is working (2023.04.24) and will be used by the questionnaire-builder
+      which will create a random array of 5-10 questions*/
+
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
--- src/app/what-to-whatch/what-to-whatch.component.ts
+++ src/app/what-to-whatch/what-to-whatch.component.ts
@@ -9,42 +9,55 @@ import { QuestionnaireBuilderService } from '../services/questionnaire-builder.s
 export class WhatToWhatchComponent implements OnInit {
 
   questions:any;
+  currentQuestion:any;
+  questionIndex:number = 0;
 
   constructor(private questionnaire:QuestionnaireBuilderService){}
 
   ngOnInit(): void {
     this.questionnaire.getQuestionnaire().subscribe(data => {
-      console.log(data);
+      this.questions = data;
+      this.currentQuestion = this.questions[this.questionIndex];
     })
   }
-
-
-
-  HCquestions = [
-    { 
-      id: 1,
-      text: 'What is your favorite color?',
-      options: [
-        { id: 1, text: 'Red' },
-        { id: 2, text: 'Blue' },
-        { id: 3, text: 'Green' }
-      ]
-    },
-    { 
-      id: 2,
-      text: 'What is your favorite food?',
-      options: [
-        { id: 1, text: 'Pizza' },
-        { id: 2, text: 'Burgers' },
-        { id: 3, text: 'Tacos' }
-      ]
-    }
-  ];
-  i = 0;
-  currentQuestion = this.HCquestions[this.i];
-  sendAnswer(id?:any){
-    this.i++;
-    this.currentQuestion = this.HCquestions[this.i];
-    console.log(id)
+  
+  logger():void {
+    console.log(this.questions)
+    console.log(this.currentQuestion)
+  }
+  
+  sendAnswer(id?:number){
+    console.log(id);
+    this.questionIndex++;
+    this.currentQuestion = this.questions[this.questionIndex];
   }
 }
+
+
+  // HCquestions = [
+  //   { 
+  //     id: 1,
+  //     text: 'What is your favorite color?',
+  //     options: [
+  //       { id: 1, text: 'Red' },
+  //       { id: 2, text: 'Blue' },
+  //       { id: 3, text: 'Green' }
+  //     ]
+  //   },
+  //   { 
+  //     id: 2,
+  //     text: 'What is your favorite food?',
+  //     options: [
+  //       { id: 1, text: 'Pizza' },
+  //       { id: 2, text: 'Burgers' },
+  //       { id: 3, text: 'Tacos' }
+  //     ]
+  //   }
+  // ];
+  // i = 0;
+  // currentQuestion = this.HCquestions[this.i];
+  // sendAnswer(id?:any){
+  //   this.i++;
+  //   this.currentQuestion = this.HCquestions[this.i];
+  //   console.log(id)
+  // }
